7fdd15a0b4af3a8d8237050852e3ff66b6078424
User <User@DESKTOP-V5ECKUA>
2023-10-28 16:58:00 +0530

    login with authentication

---
+++ Login/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Login.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context=new OfficeEntities())
+            {
+               bool isvalid= context.Users.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if (isvalid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid username and Password");
+                return View();
+            }
+            
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context=new OfficeEntities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
